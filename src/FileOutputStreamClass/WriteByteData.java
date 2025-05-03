a6a385d794458eef6961efbe66c9586956daec39
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-03 17:55:01 +0600

    data written by file output stream class

---
+++ src/FileOutputStreamClass/WriteByteData.java
@@ -0,0 +1,25 @@
+package FileOutputStreamClass;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class WriteByteData {
+    public static void main(String[] args) throws Exception {
+
+
+        try {
+            String name = "Rifat";
+
+            FileOutputStream fc = new FileOutputStream("output.txt");
+
+            byte[] nameByte = name.getBytes();
+            fc.write(nameByte);
+
+            System.out.println("Data written");
+
+
+        }
+        catch (IOException e) {
+            System.out.println(" " + e.getMessage());
+        }
+    }
+}
