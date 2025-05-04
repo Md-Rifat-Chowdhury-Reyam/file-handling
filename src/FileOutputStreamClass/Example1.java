ba67121bd64d6cc53be7fe7da656cb3e08730a39
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-04 16:23:25 +0600

    FileOutputStream

--- src/FileOutputStreamClass/Example1.java
+++ src/FileOutputStreamClass/Example1.java
@@ -6,22 +6,41 @@ public class Example1 {
     public static void main(String[] args) {
 
         String path = System.getProperty("user.dir");
-
         System.out.println("Directory " + path);
 
         try {
             FileOutputStream fc = new FileOutputStream("filehandling.txt", false);
 
+            String name = "\nRifat how are you?\n";
+            fc.write(name.getBytes());// The getBytes() method used
+            // converts a string into bytes array.
 
-            String name = "Rifat";
-            fc.write(name.getBytes());
 
-            fc.close();
+            byte[] arr = {55,56,76,37,66};
+            fc.write(arr);
+
+            byte[] array = name.getBytes();// The getBytes() method used
+            // converts a string into bytes array.
+            fc.write(array);
+
+
+            fc.flush(); //force to store all data;
+
+
+            System.out.println("Written success");
+            fc.close();//closes the file OutputStream
+
+            //cant write after close();
+            //if write then error occur
+
+
+            fc.write("I am error".getBytes());
+
         }
 
         catch (IOException e)
         {
-            System.out.println( " ");
+            System.out.println( "Error : " + e.getMessage());
         }
     }
 }
